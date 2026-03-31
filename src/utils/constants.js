1bc6a16f4f9e5cdee2475c5c2333a306eb017fc0
akshay-khatke <khatkeakki@gmail.com>
2026-03-31 11:22:34 +0530

    changes of cors and localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
-// export const BASE_URL = "http://localhost:3000"
-export const BASE_URL = "https://devtinderbe-3.onrender.com"
+export const BASE_URL = location.hostname === "localhost"
+    ? "http://localhost:10000"
+    : "https://devtinderbe-3.onrender.com";
+
+export const RAZORPAY_KEY_ID = "rzp_test_YOUR_KEY_HERE";
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,13 +5,9 @@ let socket;
 
 export const createSocketConnection = () => {
     if (!socket) {
-        if (location.hostname === "localhost") {
-            socket = io("http://localhost:3000");
-        } else {
-            socket = io(BASE_URL, {
-                withCredentials: true
-            });
-        }
+        socket = io(BASE_URL, {
+            withCredentials: true
+        });
     }
     return socket;
 };
