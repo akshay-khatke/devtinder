032cc2f6f482785d0454f3db6bc3dc8a00635203
akshay-khatke <khatkeakki@gmail.com>
2026-08-21 09:16:35 +0530

    for deployement on aws changed baseurl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
-export const BASE_URL = location.hostname === "localhost"
-    ? "http://localhost:10000"
-    : "https://devtinderbe-3.onrender.com";
+// export const BASE_URL = location.hostname === "localhost"
+//     ? "http://localhost:10000"
+//     : "https://devtinderbe-3.onrender.com";
+
+export const BASE_URL = "http://localhost:10000"
+
 
 export const RAZORPAY_KEY_ID = "rzp_test_YOUR_KEY_HERE";
