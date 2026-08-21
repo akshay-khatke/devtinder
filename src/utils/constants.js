cf327b32c51f4e59fa248dded4f1f852646183e1
akshay-khatke <khatkeakki@gmail.com>
2026-08-21 10:01:01 +0530

    changes of contant files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || (location.hostname === "localhost"
+export const BASE_URL = process.env.VITE_BASE_URL || (location.hostname === "localhost"
     ? "http://localhost:10000"
     : "https://devtinderbe-3.onrender.com");
 
-export const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID;
+export const RAZORPAY_KEY_ID = process.env.VITE_RAZORPAY_KEY_ID;
